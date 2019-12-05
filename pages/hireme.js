4a15c730fddb2b4c5b0eb3583d477371b70ea4fa
marcush12 <marcos012santos@gmail.com>
2019-12-05 13:01:03 -0300

    terceiro commit

---
+++ pages/hireme.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const HireMe = () => {
+  return (
+    <Layout title="Hire Me">
+
+      <p>You can hire me at <a href="mailto:annette@heaven.com">annette heaven</a></p>
+    </Layout>
+  )
+}
+
+export default HireMe
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const Index = () => {
+  return (
+    <Layout title="Home">
+
+    <p>Bem-vindo a mais um curso!</p>
+    </Layout>
+  )
+}
+
+export default Index
